d3125d17b3d147355b8b7413e8405615630a2ad8
neptuno1981 <neptunos24@gmail.com>
2021-06-06 19:10:48 -0400

    Update config.js

--- database/config.js
+++ database/config.js
@@ -6,7 +6,7 @@ const dbConnection = async() => {
 
     try {
 
-        await mongoose.connect( process.env.MONGODB_CNN1, {
+        await mongoose.connect( process.env.MONGODB_CNN, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true,
